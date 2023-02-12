d343eb9320d2289970e1ae01f4b51bcac3396a3d
Pavel4090 <pavelcurikov090@gmail.com>
2023-02-12 14:55:00 +0300

    WebDriver tests with inputs

--- src/main/java/WebDriver/Inputs.java
+++ src/main/java/WebDriver/Inputs.java
@@ -3,15 +3,32 @@ package WebDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class Inputs extends BaseTest{
 
     @Test
-    public void operationsWithElements() {
+    public void keysArrowUPDown() {
         driver.get("https://the-internet.herokuapp.com/inputs");
         WebElement element = driver.findElement(By.tagName("input"));
         element.click();
+        Keys arrowUp = Keys.ARROW_UP;
+        Keys arrowDown = Keys.ARROW_DOWN;
+        element.sendKeys(arrowUp);
+        element.sendKeys(arrowUp);
+        element.sendKeys(arrowUp);
+        element.sendKeys(arrowUp);
+        element.sendKeys(arrowUp);
+        element.sendKeys(arrowDown);
+        element.sendKeys(arrowDown);
+        element.sendKeys(arrowDown);
+        element.clear();
+        element.sendKeys("123456");
+        String result = element.getText();
+        System.out.println(result);
+        element.clear();
+        element.sendKeys("Input test");
+        String result2 = element.getText();
+        System.out.println(result2);
     }
 }
